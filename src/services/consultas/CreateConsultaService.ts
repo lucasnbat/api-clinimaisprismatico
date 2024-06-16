e33f3d9dd586f205a390b08886d0b6c74a3dad60
lucasnbat <lucasnunesb6@gmail.com>
2024-06-16 01:50:31 -0400

    tratamento de TimeZone adicionado para considerar o timeZone America/cuiaba, ou seja, fuso horário do mato grosso

--- src/services/consultas/CreateConsultaService.ts
+++ src/services/consultas/CreateConsultaService.ts
@@ -1,19 +1,10 @@
-// id                 Int       @id @default(autoincrement())
-// medico_id          Int
-// paciente_id        Int
-// data_consulta      DateTime
-// horario            String
-// outras_informacoes String
-// is_cancelada       Boolean
-// created_at         DateTime? @default(now())
-// updated_at         DateTime? @default(now())
-
 import prismaClient from "../../prisma";
+import { format, toZonedTime } from 'date-fns-tz';
 
 interface ConsultaRequest {
     medico_id: number,
     paciente_id: number,
-    data_consulta: Date,
+    data_consulta: string,  
     horario: string,
     outras_informacoes: string,
     is_cancelada?: boolean,
@@ -27,11 +18,18 @@ class CreateConsultaService {
         horario,
         outras_informacoes,
     }: ConsultaRequest) {
+        // Combine data e horário para criar um objeto Date
+        const zonedDateTime = new Date(`${data_consulta}T${horario}`);
+        // Converta o objeto Date para o fuso horário 'America/Cuiaba'
+        const zonedTime = toZonedTime(zonedDateTime, 'America/Cuiaba');
+        // Formate a data para o formato UTC antes de salvar no banco
+        const dataConsultaUtc = format(zonedTime, "yyyy-MM-dd'T'HH:mm:ss.SSSxxx", { timeZone: 'UTC' });
+
         const consulta = await prismaClient.consultas.create({
             data: {
                 medico_id: medico_id,
                 paciente_id: paciente_id,
-                data_consulta: data_consulta,
+                data_consulta: new Date(dataConsultaUtc),  // Use a data em UTC
                 horario: horario,
                 outras_informacoes: outras_informacoes,
             }
