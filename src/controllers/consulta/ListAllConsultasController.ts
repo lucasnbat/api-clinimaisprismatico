6d697016e32c70fde79b65c0427f9e43297fbfb3
rennata <rennata.arruda.santos@unemat.br>
2024-06-16 14:43:05 -0400

    ajuste listar todas as consultas - com filtro

--- src/controllers/consulta/ListAllConsultasController.ts
+++ src/controllers/consulta/ListAllConsultasController.ts
@@ -3,9 +3,14 @@ import { ListAllConsultasService } from "../../services/consultas/ListAllConsult
 
 class ListAllConsultasController {
     async handle(request: Request, response: Response) {
+        const { medico_id, data_consulta, paciente_id } = request.body;
         const listAllConsultasService = new ListAllConsultasService();
 
-        const consultas = await listAllConsultasService.execute();
+        const consultas = await listAllConsultasService.execute({
+            medico_id: Number(medico_id),
+            data_consulta: data_consulta as string,
+            paciente_id: Number(paciente_id),
+        });
 
         return response.json(consultas);
     }
--- src/services/consultas/ListAllConsultasService.ts
+++ src/services/consultas/ListAllConsultasService.ts
@@ -1,8 +1,24 @@
 import prismaClient from "../../prisma";
 
+interface ConsultaFiltosRequest {
+    medico_id: number;
+    data_consulta: string; // formato: YYYY-MM-DD
+    paciente_id: number;
+}
+
 class ListAllConsultasService {
-    async execute() {
-        const consultas = await prismaClient.consultas.findMany();
+    async execute({ medico_id, data_consulta, paciente_id }: ConsultaFiltosRequest) {
+        const consultas = await prismaClient.consultas.findMany({
+            where: {
+                medico_id: medico_id,
+                data_consulta: new Date(data_consulta),
+                paciente_id: paciente_id,
+            },
+            include: {
+                medico: true,
+                paciente: true,
+            }
+        });
 
         return consultas;
     }
