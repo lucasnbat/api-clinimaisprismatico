4fe378a49c51d1b6d4aefcc1120d85ce5fedec2e
lucasnbat <lucasnunesb6@gmail.com>
2024-06-20 08:04:30 -0400

    fix: ajuste em serviço de consulta por id

--- src/services/consultas/ListConsultaByIdService.ts
+++ src/services/consultas/ListConsultaByIdService.ts
@@ -2,7 +2,7 @@ import prismaClient from "../../prisma";
 
 class ListConsultaByIdService {
     async execute(consulta_id:number) {
-        const consulta = prismaClient.consultas.findUnique({
+        const consulta = await prismaClient.consultas.findUnique({
             where: {
                 id: consulta_id,
             }
